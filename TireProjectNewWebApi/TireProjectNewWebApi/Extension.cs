f923ceb32a705f7cc37d1ce2bd35e28a3646a99a
zohaibshahzad1990 <zohaibshazad1990@gmail.com>
2025-03-14 00:04:04 +0500

    left files

---
+++ TireProjectNewWebApi/TireProjectNewWebApi/Extension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TireProjectNewWebApi
+{
+    public static class Extensions
+    {
+        public static string ReplaceDomain(this string url)
+        {
+            if (Uri.TryCreate(url, UriKind.Absolute, out Uri? createdURI))
+            {
+                if (createdURI is not null)
+                {
+                    UriBuilder uriBuilder = new UriBuilder(createdURI)
+                    {
+                        Scheme = Uri.UriSchemeHttp, // Changing to http
+                        Host = "3.133.136.76",
+                        Port = -1 // Removing the port
+                    };
+                    return uriBuilder.Uri.ToString();
+                }
+                else
+                {
+                    return url;
+                }
+                
+            }
+            else
+            {
+                return url;
+            }
+
+        }
+    }
+}
