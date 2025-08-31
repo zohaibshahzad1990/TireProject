c7ab02d5c765b3080f80613ef3823fc9edd24861
zohaibshahzad1990 <zohaibshazad1990@gmail.com>
2025-09-01 00:01:40 +0500

    linker work done

---
+++ TireProject.Android/LinkerPleaseInclude.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TireProject.Droid
+{
+    public class LinkerPleaseInclude
+    {
+        public void IncludePdfSharpFonts()
+        {
+            // Force the linker to keep these types
+            var marker = typeof(PdfSharp.Fonts.IFontResolverMarker);
+        }
+    }
+}
--- TireProject/Constants.cs
+++ TireProject/Constants.cs
@@ -6,7 +6,7 @@ namespace TireProject
 {
     public class Constants
     {
-        //public static string BASE_URL= "http://3.133.136.76";
-        public static string BASE_URL = "https://af98-2a09-bac1-5b00-8-00-3b6-a.ngrok-free.app";
+        public static string BASE_URL= "http://3.133.136.76";
+        //public static string BASE_URL = "https://af98-2a09-bac1-5b00-8-00-3b6-a.ngrok-free.app";
     }
 }
